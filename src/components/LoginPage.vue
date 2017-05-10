16360abffedc477e7643295b9abfb6078259777d
Aleksei Drokin <aleksei.drokin@eastview.com>
2017-05-10 12:33:19 -0500

    initial commint

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,29 @@
+<template>
+ <main-template>
+   <div slot="header">It's a Header</div>
+   <section>
+      <h1>Main content goes here.</h1>
+      <p>bang bang!</p>
+    </section>
+    <footer slot="footer">It's a Footer</footer>
+ </main-template>
+</template>
+
+<script>
+import MainTemplate from './MainTemplate';
+
+export default {
+  components: {
+    MainTemplate,
+  },
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/MainTemplate.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <slot name="header"></slot>
+    <slot/>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  @import "../assets/styles/global-styles.less";
+
+</style>
